83a71813b317bc92038857d48ddcc7ed0c9362a4
Felipe Garcia Fogal <felipefogal@gmail.com>
2026-09-18 11:19:12 -0300

    Concluindo parte 1 das configurações de Authentication

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,8 +1,13 @@
-import auth from "express-oauth2-jwt-bearer";
+import { auth } from "express-oauth2-jwt-bearer";
+import dotenv from "dotenv/config.js";
+
+const audienceRoute = process.env.AUTH0_AUDIENCE
 
 const checkJwt = auth({
-  audience: process.env.AUTH0_AUDIENCE,
+  audience: audienceRoute,
+  // audience: 'https://dev-i34u0vgofp0k1gx0.us.auth0.com/api/v2/',
   issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
+  tokenSigningAlg: "RS256",
 });
 
-export default checkJwt;
+export { checkJwt };
